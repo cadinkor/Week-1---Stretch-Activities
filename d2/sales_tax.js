399396edba00286b2253f385df25d13c8aba2c5b
Elliott Thomlison <elliott.thomlison@gmail.com>
2021-03-03 19:30:59 -0800

    reviewing and adjusting

---
+++ d2/sales_tax.js
@@ -0,0 +1,41 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+function calculateSalesTax() {
+
+  var result = {};
+
+  companySalesData.forEach((company) => {
+    var transientRevenue = 0;
+    for (var revenue of company.sales) {
+      (!result[company.name]) ? result[company.name] = {totalSales: revenue, totalTaxes: 0} : result[company.name].totalSales += revenue;
+      transientRevenue += revenue;
+    }
+  result[company.name]['totalTaxes'] += (transientRevenue * salesTaxRates[company.province]);
+  });
+  return result;
+}
+
+var taxSummary = calculateSalesTax();
+console.log(taxSummary);
